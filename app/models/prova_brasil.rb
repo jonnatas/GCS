8825cd45951b99f4934516fa86592c737491361a
Gabriel Araújo <gabrieldea.silva@gmail.com>
2015-04-22 11:19:00 -0300

    Refactoring how to access Prova Brasil report

--- app/models/prova_brasil.rb
+++ app/models/prova_brasil.rb
@@ -1,6 +1,53 @@
 class ProvaBrasil < ActiveRecord::Base
-	
+
+	attr_accessor :prova_brasil_request_result, :portuguese_average_score,
+	:math_average_score
+
+
+	def initialize(year, id_grade, id_state,final_year)
+		@year = year
+		@id_grade = id_grade
+		@id_state = id_state
+		@final_year = final_year
+		request_prova_brasil_report
+	end
+
+	def request_prova_brasil_report
+		@prova_brasil_request_result = Array.new
+
+		(@year..@final_year).each do |year|
+			current_prova_brasil = request_prova_brasil(year,@id_state,@id_grade)
+			@prova_brasil_request_result.push(current_prova_brasil)
+		end
+		request_average_to_portuguese
+		request_average_to_math
+	end
+	private :request_prova_brasil_report
+
+	def request_average_to_portuguese
+		total_years = @final_year - @year
+		total_score = 0.0
+
+		@prova_brasil_request_result.each do |current_prova_brasil|
+			total_score += current_prova_brasil.portuguese_score
+		end
+		@portuguese_average_score = total_score/total_years
+	end
+	private :request_average_to_portuguese
+
+	def request_average_to_math
+		total_years = @final_year - @year
+		total_score = 0.0
+
+		@prova_brasil_request_result.each do |current_prova_brasil|
+			total_score += current_prova_brasil.math_score
+		end
+		@math_average_score = total_score/total_years
+	end
+	private :request_average_to_math
+
 	def request_prova_brasil(year, id_state, id_grade)
 		return ProvaBrasil.where(:year => year,:id_grade => id_grade, :id_state => id_state).first
 	end
+	private :request_prova_brasil_report
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,50 +1,21 @@
 class Report
+
+	attr_accessor :prova_brasil_request_result, :portuguese_average_score,
+	:math_average_score, :rate_request_result
+
+
 	def initialize(year, id_grade, id_state,final_year)
 		@year = year
 		@id_grade = id_grade
 		@id_state = id_state
 		@final_year = final_year
+		request_report
 	end
 
 	def request_report
 		request_prova_brasil_report
 		request_rate_report
 	end
-
-	def request_prova_brasil_report
-		prova_brasil_dao = ProvaBrasil.new
-
-		@prova_brasil_request_result = Array.new
-
-		(@year..@final_year).each do |year|
-			current_prova_brasil = prova_brasil_dao.request_prova_brasil(year,@id_state,@id_grade)
-			@prova_brasil_request_result.push(current_prova_brasil)
-		end
-		request_average_to_portuguese
-		request_average_to_math
-	end
-	private :request_prova_brasil_report
-
-	def request_average_to_portuguese
-		total_years = @final_year - @year
-		total_score = 0.0
-
-		@prova_brasil_request_result.each do |current_prova_brasil|
-			total_score += current_prova_brasil.portuguese_score
-		end
-		return total_score/total_years
-	end
-
-	def request_average_to_math
-		total_years = @final_year - @year
-		total_score = 0.0
-
-		@prova_brasil_request_result.each do |current_prova_brasil|
-			total_score += current_prova_brasil.math_score
-		end
-		return total_score/total_years
-	end
-
 
 	def request_rate_report
 		rate_dao = Report.new
@@ -57,6 +28,5 @@ class Report
 		end
 	end
 	private :request_rate_report
-
 
 end
