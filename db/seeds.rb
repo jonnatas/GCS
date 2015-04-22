4fca888d7bbc29fc4edc619dd8104d4870d4baa5
Gabriel Araújo <gabiru1296@gmail.com>
2015-04-21 23:42:53 -0300

    Creating DB example data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+brazil_test = ProvaBrasil.create(id_test: 1, id_grade: 1, total_students: 30, portuguese_score: 6.6, math_score: 6.7,
+		year: 2005, id_state: 1)
+
+	rate = Rate.create(evasion: 15,performance: 10, distortion: 13, year: 2005, id_grade: 1, id_state: 1)
