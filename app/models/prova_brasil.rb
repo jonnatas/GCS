8b944b4d6cbfab4135b6056b91ebf0618807a2ee
Gabriel Araújo <gabiru1296@gmail.com>
2015-04-22 01:00:06 -0300

    Adding request ProvaBrasil and Rate objects from DB at DAOs

--- app/models/prova_brasil.rb
+++ app/models/prova_brasil.rb
@@ -1,2 +1,6 @@
 class ProvaBrasil < ActiveRecord::Base
+	
+	def request_prova_brasil(year, id_state, id_grade)
+		return ProvaBrasil.where(:year => year,:id_grade => id_grade, :id_state => id_state).first
+	end
 end
--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,2 +1,5 @@
 class Rate < ActiveRecord::Base
+	def request_rate(year,id_state,id_grade)
+		return Rate.where(:year => year, :id_state => id_state, :id_grade => id_grade).first
+	end
 end
