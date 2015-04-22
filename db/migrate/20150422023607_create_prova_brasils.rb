2caa6536b2aca0bb8ad327bfda429975df3ab4ea
Gabriel Araújo <gabiru1296@gmail.com>
2015-04-21 23:38:09 -0300

    Creating DB columns at migration

--- db/migrate/20150422023607_create_prova_brasils.rb
+++ db/migrate/20150422023607_create_prova_brasils.rb
@@ -2,6 +2,14 @@ class CreateProvaBrasils < ActiveRecord::Migration
   def change
     create_table :prova_brasils do |t|
 
+    	t.integer :id_test
+    	t.integer :id_grade
+    	t.integer :total_students
+    	t.float :portuguese_score
+    	t.float :math_score
+    	t.integer :year
+    	t.integer :id_state
+    	
       t.timestamps
     end
   end
