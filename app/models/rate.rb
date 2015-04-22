f8cc3c643c32b43f1900cb6e9e3f9ab28aa50216
Gabriel Araújo <gabrieldea.silva@gmail.com>
2015-04-22 11:28:09 -0300

    Refactoring access to Rate data

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,5 +1,28 @@
 class Rate < ActiveRecord::Base
+
+	attr_accessor :rate_request_result
+
+	def initialize(year, id_grade, id_state,final_year)
+		@year = year
+		@id_grade = id_grade
+		@id_state = id_state
+		@final_year = final_year
+		request_rate_report
+	end
+
+	def request_rate_report
+		@rate_request_result = Array.new
+
+		(@year..@final_year).each do |year|
+			current_rate = rate_dao.request_rate(year,@id_state,@id_grade)
+			@rate_request_result.push(current_rate)
+		end
+	end
+	private :request_rate_report
+
+
 	def request_rate(year,id_state,id_grade)
 		return Rate.where(:year => year, :id_state => id_state, :id_grade => id_grade).first
 	end
+	private :request_rate
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,8 +1,6 @@
 class Report
 
-	attr_accessor :prova_brasil_request_result, :portuguese_average_score,
-	:math_average_score, :rate_request_result
-
+	attr_accessor :prova_brasil, :rates
 
 	def initialize(year, id_grade, id_state,final_year)
 		@year = year
@@ -13,20 +11,9 @@ class Report
 	end
 
 	def request_report
-		request_prova_brasil_report
-		request_rate_report
+		@prova_brasil = ProvaBrasil.new(@year,@id_grade,@id_state,@final_year)
+		@rates = Rate.new(@year,@id_grade,@id_state,@final_year)
 	end
-
-	def request_rate_report
-		rate_dao = Report.new
-
-		@rate_request_result = Array.new
-
-		(@year..@final_year).each do |year|
-			current_rate = rate_dao.request_rate(year,@id_state,@id_grade)
-			@rate_request_result.push(current_rate)
-		end
-	end
-	private :request_rate_report
+	private :request_report
 
 end
