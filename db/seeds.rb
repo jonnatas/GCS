cb3aa5ee3b28e0a85c342720ef93ce1cc0402b1b
Gabriel Araújo <gabiru1296@gmail.com>
2015-04-22 00:58:50 -0300

    Creating more data example for DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-brazil_test = ProvaBrasil.create(id_test: 1, id_grade: 1, total_students: 30, portuguese_score: 6.6, math_score: 6.7,
+ 	ProvaBrasil.create(id_test: 1, id_grade: 1, total_students: 30, portuguese_score: 6.6, math_score: 6.7,
 		year: 2005, id_state: 1)
 
-	rate = Rate.create(evasion: 15,performance: 10, distortion: 13, year: 2005, id_grade: 1, id_state: 1)
+ 	ProvaBrasil.create(id_test: 2, id_grade: 1, total_students: 30, portuguese_score: 7.0, math_score: 4.7,
+		year: 2006, id_state: 1)
+
+ 	ProvaBrasil.create(id_test: 3, id_grade: 1, total_students: 30, portuguese_score: 9.6, math_score: 8.7,
+		year: 2007, id_state: 1)
+
+
+	Rate.create(evasion: 15,performance: 10, distortion: 13, year: 2005, id_grade: 1, id_state: 1)
+	Rate.create(evasion: 10,performance: 9, distortion: 13, year: 2006, id_grade: 1, id_state: 1)
+	Rate.create(evasion: 12,performance: 8, distortion: 13, year: 2007, id_grade: 1, id_state: 1)
