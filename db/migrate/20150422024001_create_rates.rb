8e8b6a5051ec94ed7c8f73693a9e0a71dff97793
Gabriel Araújo <gabiru1296@gmail.com>
2015-04-21 23:41:24 -0300

    Adding DB columns at migration

--- db/migrate/20150422024001_create_rates.rb
+++ db/migrate/20150422024001_create_rates.rb
@@ -2,6 +2,12 @@ class CreateRates < ActiveRecord::Migration
   def change
     create_table :rates do |t|
 
+    	t.float :evasion
+    	t.float :performance
+    	t.float :distortion
+    	t.integer :year
+    	t.integer :id_grade
+    	t.integer :id_state
       t.timestamps
     end
   end
