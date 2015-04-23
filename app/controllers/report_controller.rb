da101adf9152d57f5c88d32a844d47a4921425d5
Gabriel Araújo <gabrieldea.silva@gmail.com>
2015-04-23 00:46:55 -0300

    Creating interface for request report from view to model at Report controller

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,2 +1,12 @@
 class ReportController < ApplicationController
+
+	def index
+		
+	end
+
+	def request_report
+		@report = Report.new(params[:year],params[:id_grade],params[:id_state],params[:final_year])
+	end
+
+
 end
