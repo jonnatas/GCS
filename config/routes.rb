51ab21f60ca577b40ff9640d1e03cdacec3015c9
thiagotmb <tmb0710@gmail.com>
2015-04-29 10:22:56 -0300

    Creating StatiPages Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'static_pages/home'
+
   get ':controller(/:action(.:format)(/:id))'
 
   # The priority is based upon order of creation: first created -> highest priority.
