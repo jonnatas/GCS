53ed2b1d0a77b1e2425a3cd101ba94b7499d398f
Matheus Toscano <ssstoscano@gmail.com>
2015-04-29 08:10:59 -0300

    Show a static error page based on the exception raised

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  #
   protected
   def local_request?
     false
   end	
+
+  def rescue_action_in_public(exception)
+    case exception
+    when ActiveRecord::RecordNotFound
+      render :file => "#{RAILS_ROOT}/public/404.html", :status => 404
+    else
+  end
 
 end
