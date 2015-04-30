7850909dda3c0975f117c6652047c7f5300446a0
thiagotmb <tmb0710@gmail.com>
2015-04-30 11:49:46 -0300

    adding link to report in home

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   root 'static_pages#home'
-  # get 'report' => 'report/index'
   get ':controller(/:action(.:format)(/:id))'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
