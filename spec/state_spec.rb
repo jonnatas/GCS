ce33af8fdb636521d3d668f9481ef3cfaf4519d2
thiagotmb <tmb0710@gmail.com>
2015-05-01 14:30:35 -0300

    Installing rspec gem and testing state model

---
+++ spec/state_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require_relative'../app/models/state'
+
+describe State do
+  before :each do
+    @state_id = State.id_by_description("AC")
+  end
+
+  describe "#id_by_description"
+    it "takes description parameter and return a state id" do
+      expect(@state_id).to eq(1)
+    end
+
+end
