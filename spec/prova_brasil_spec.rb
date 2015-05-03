dfb4366de6b22a9264b2a3e4687944044dc33af1
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-05-03 09:41:01 -0300

    making the results on the tests to be correct and adding compute variance test on rate_spec test

--- spec/prova_brasil_spec.rb
+++ spec/prova_brasil_spec.rb
@@ -12,6 +12,7 @@ describe ProvaBrasil do
 
   describe "#initialize"
   it "take the params and set them to the object" do
+    @final_year = ProvaBrasil.final_year_avaiable(@year,@id_grade,@id_state)
     expect(@prova_brasil).not_to eq(nil)
     expect(@prova_brasil.instance_variable_get(:@year)).to eq(@year)
     expect(@prova_brasil.instance_variable_get(:@id_grade)).to eq(@id_grade)
@@ -22,9 +23,15 @@ describe ProvaBrasil do
   describe "#final_year_avaiable"
     it "take the state, grade and year and return the final year avaiable for the current prova_brasil" do
       @final_year = ProvaBrasil.final_year_avaiable(@year,@id_grade,@id_state)
-      expect(@final_year).to eq("2010")
+      expect(@final_year).to eq("2011")
     end
 
+  describe "#compute_variance"
+    it "Verify if the variance calculus is correct" do
+      variance = @prova_brasil.compute_variance([0.0,3.6,5.6,6.6,6.6,7.0,8.6,9.6])
+      expect(variance.round(4)).to eq(7.9175)
+    end
 
+  
 
 end
