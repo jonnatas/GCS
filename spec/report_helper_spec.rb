5501f8388009e4d4e664d54451a5f609b3f4bf7b
João Vítor de Brito Fernandes <fernandesjvb@gmail.com>
2015-05-04 20:45:10 -0300

    Adding test for average computing method

--- spec/report_helper_spec.rb
+++ spec/report_helper_spec.rb
@@ -3,6 +3,12 @@ require_relative '../app/helpers/report_helper'
 
 describe ReportHelper do
 
+  describe "#compute_average_for"
+  it "take an array with data and return the average" do
+    average = ReportHelper.compute_average_for([1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0])
+    expect(average).to eq(5.5)
+  end
+
   describe "#compute_standard_deviation"
   it "take an array with data and return the standar deviation calc of this data" do
     deviation = ReportHelper.compute_standard_deviation([0.0,3.6,5.6,6.6,6.6,7.0,8.6,9.6])
