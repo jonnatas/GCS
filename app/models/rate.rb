fde9bd546ed19c9f44038d213da87a670a90bcd0
Gabriel Araújo <gabrieldea.silva@gmail.com>
2015-05-05 10:36:28 -0300

    Testing request_rate at Rate Model

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -136,5 +136,4 @@ class Rate < ActiveRecord::Base
 	def request_rate(year,id_state,id_grade)
 		return Rate.where(:year => year, :id_state => id_state, :id_grade => id_grade).first
 	end
-	private :request_rate
 end
--- spec/rate_spec.rb
+++ spec/rate_spec.rb
@@ -17,5 +17,19 @@ describe Report do
 		it "Find the last year available" do
 			expect(@rate.instance_variable_get(:@final_year)).to eq("2012")
 		end
+
+	describe "#request_rate"
+	    it "Take de state, grade and year and return a Rate object from DB" do
+	      new_rate = @rate.request_rate(2005,1,1)
+	      expect(new_rate.id).to eq(1)
+	      expect(new_rate.evasion).to eq(15)
+	      expect(new_rate.performance).to eq(10)
+	      expect(new_rate.distortion).to eq(13)
+	      expect(new_rate.id_grade).to eq(1)
+	      expect(new_rate.year).to eq(2005)
+	      expect(new_rate.id_state).to eq(1)
+	    end
+
+	
   
 end
