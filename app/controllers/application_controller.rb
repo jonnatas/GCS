37c802cd5bd2cadb2ded8c9801ded416d1ffc66a
thiagotmb <tmb0710@gmail.com>
2015-05-05 23:05:13 -0300

    Adding unmetched_rout rescue excpetion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
-	
+  rescue_from Exception, :with => :render_error
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
+  rescue_from ActionController::RoutingError, :with => :render_not_found
+
   def index
   end
-  #Overriding the local_request? method
-  protected
-  def local_request?
-    false
-  end	
 
-  def rescue_action_in_public(exception)
-    case exception
-      when ActiveRecord::RecordNotFound
-	flash[:error] = "Page doesn't exist!"
-        render :file => "#{RAILS_ROOT}/public/404.html", :status => 404
-      else
-        super
+  #called by last route matching unmatched routes.  Raises RoutingError which will be rescued from in the same way as other exceptions.
+  def raise_not_found!
+    raise ActionController::RoutingError.new("No route matches #{params[:unmatched_route]}")
+  end
+
+  #render 404 error
+  def render_not_found(e)
+    respond_to do |f|
+      f.html{ render :template => "errors/404", :status => 404 }
+      f.js{ render :partial => "errors/ajax_404", :status => 404 }
     end
   end
 
