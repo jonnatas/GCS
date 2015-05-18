62ff76b0855b2ae9777b37072e3ba8d7a32a83e8
Jhonatan Alves <jhon_fla@hotmail.com>
2015-05-18 20:41:31 -0300

    Applying an indentation pattern in the helpers

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -1,35 +1,34 @@
 module ReportHelper
 
-  def self.compute_average_for(data)
-    total_score = 0.0
-    data.each do |current_data|
-      total_score += current_data
-    end
-    return total_score/data.count
-  end
+	def self.compute_average_for(data)
+		total_score = 0.0
+		data.each do |current_data|
+			total_score += current_data
+		end
+		return total_score/data.count
+	end
 
-  def self.compute_standard_deviation(data)
-    average = compute_average_for(data)
-    total_variance = 0.0
-    data.each do |current_data|
-      total_variance += (current_data - average)**2
-    end
+	def self.compute_standard_deviation(data)
+		average = compute_average_for(data)
+		total_variance = 0.0
+		data.each do |current_data|
+			total_variance += (current_data - average)**2
+		end
 
-    total_variance = total_variance/data.count
-    standard_deviation 	= total_variance**(0.5)
-    return standard_deviation
+		total_variance = total_variance/data.count
+		standard_deviation 	= total_variance**(0.5)
+		return standard_deviation
 
-  end
+	end
 
-  def self.compute_variance(data)
-    average = compute_average_for(data)
-    total_variance = 0.0
-    data.each do |current_data|
-      total_variance += (current_data - average)**2
-    end
+	def self.compute_variance(data)
+		average = compute_average_for(data)
+		total_variance = 0.0
+		data.each do |current_data|
+			total_variance += (current_data - average)**2
+		end
 
-    return total_variance/data.count
-  end
-
+		return total_variance/data.count
+	end
 
 end
