8aedc8604661adec896e87a804aa0599f57f75e0
João Vítor de Brito Fernandes <fernandesjvb@gmail.com>
2015-05-25 18:08:21 -0300

    Initializing ranking model

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,4 +1,8 @@
 class RankingController < ApplicationController
   def index
   end
+
+  def request_ranking
+  	@ranking = Ranking.new(params[:year],params[:grade],params[:test_type],params[:local],params[:rate])
+  end
 end
