05a96d9581d15aadf21acff9a2ce64302a98c3a3
Thiago Bernardes <tmb0710@gmail.com>
2015-05-26 18:21:48 -0300

    Fixing exceptions handling in compare reports
    Signed-off-by: Thiago Bernardes <tmb0710@gmail.com>
    Signed-off-by: PedroSales <pedrosal3s@gmail.com>
    Signed-off-by: Gustavo Cavalcante Oliveira <gustavo.cavalcante.oliveira@live.com>

--- app/controllers/compare_reports_controller.rb
+++ app/controllers/compare_reports_controller.rb
@@ -6,7 +6,7 @@ class CompareReportsController < ApplicationController
 
 	def request_comparation
 
-		@first_report = verify_params_of_test_type(params[:first_year],params[:grade],params[:second_state],params[:first_test_type],params[:first_public_type],params[:first_local])
+		@first_report = verify_params_of_test_type(params[:first_year],params[:grade],params[:first_state],params[:first_test_type],params[:first_public_type],params[:first_local])
 		@first_report.request_report
 
 		@second_report = verify_params_of_test_type(params[:second_year],params[:grade],params[:second_state],params[:second_test_type],params[:second_public_type],params[:second_local])
