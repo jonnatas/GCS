ef7cc89b968662843fcc6fe4bc634304d460942b
Jhonatan Alves <jhon_fla@hotmail.com>
2015-05-29 17:46:10 -0300

    Refactoring report controller

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -5,21 +5,28 @@ class ReportController < ApplicationController
 	end
 
 	def request_report
-		if params[:test_type] == "Publica"
-			if params[:public_type] == "Total"
-				@report = Report.new(params[:year],params[:grade],params[:state],"Publico",params[:local]) 
-			else
-				@report = Report.new(params[:year],params[:grade],params[:state],params[:public_type],params[:local]) 
-			end
-		else
-			@report = Report.new(params[:year],params[:grade],params[:state],params[:test_type],params[:local])
-		end
+		test_type_analyzer
+
+		@report = Report.new(params[:year],params[:grade],params[:state],@test_type,params[:local])
+
 		@report.request_report
 
 		respond_to do |format|
 			format.json { render json: @report.report_result_hash}
 		end
 		puts @report.report_result_hash
+	end
+
+	def test_type_analyzer
+		if params[:test_type] == "Publica"
+			if params[:public_type] == "Total"
+				@test_type = "Publico"
+			else
+				@test_type = params[:public_type]
+			end
+		else
+			@test_type = params[:test_type]
+		end
 	end
 
 end
