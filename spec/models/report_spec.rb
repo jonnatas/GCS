ac487d19a34958b3debfa5af4f8daf1572fd10eb
Matheus <teustoscano@gmail.com>
2015-06-01 12:45:17 -0300

    Writing test for the rate model,and refactoring the code

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -5,7 +5,7 @@ describe Report do
 
  before :each do
  	@report = Report.new(2011,"1° ano", "AC", "Total", "Total")
- end 
+ end
 
 describe "#initialize"
   it "take the params and set them to the object" do
@@ -14,11 +14,11 @@ describe "#initialize"
      expect(@report.instance_variable_get(:@state_id)).to eq(1)
   end
 
-describe "#request_report" 
+describe "#request_report"
 	it "create prova_brasil and rate object and reporturn a hash with this objects and year" do
 		@report.request_report
 		expect(@report.report_result_hash).not_to eq(nil)
-	end  
+	end
 
 
 
