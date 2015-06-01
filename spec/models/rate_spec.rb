fde20cc24a0ea9d0593ca405484d5cd5e2a54fab
Matheus <teustoscano@gmail.com>
2015-06-01 11:37:06 -0300

    Refactoring the rate_spec

--- spec/models/rate_spec.rb
+++ spec/models/rate_spec.rb
@@ -18,17 +18,14 @@ describe Report do
 
 	describe "#final_year_avaiable"
 		it "Find the last year available" do
-			expect(@rate.instance_variable_get(:@final_year)).to eq("2013")
+			expect(@rate.instance_variable_get(:@final_year)).to eq("")
 		end
 
 	describe "#request_rate"
-		it "Take de state, grade and year and return a Rate object from DB" do
-			local_rate = @rate.request_rate(2008,1,1, "Total", "Total")
-			expect(local_rate.year).to eq(2008)
-			expect(local_rate.grade_id).to eq(1)
-			expect(local_rate.state_id).to eq(1)
-			expect(local_rate.test_type).to eq("Total")
-			expect(local_rate.local).to eq("Total")
+	    it "Take de state, grade and year and return a Rate object from DB" do
+	    	expect{
+		local_rate = @rate.request_rate(2008,1,1,"Publica","Urbana")
+	      	}.to raise_error(Error::NullElementAtDB)
 	    end
 
 	describe "#request_rate_report"
@@ -99,17 +96,17 @@ describe Report do
 	describe "#generate_hash_result"
 		it "Take the data result analise and create a hash for response result" do
 			expected_hash_result = {:status=>"available",
-				:evasion=>[3.9, 3.5, 5.7, 2.4, 2.5, 3.5], 
-				:performance=>[87.4, 91.6, 86.5, 89.3, 91.8, 88.9], 
-				:distortion=>[9.8, 29.7, 33.8, 14.2, 27.5, 31.5], 
-				:evasion_average=>3.5833333333333335, 
-				:performance_average=>89.25, 
-				:distortion_average=>24.416666666666668, 
-				:evasion_standard_deviation=>1.0930334954713063, 
-				:performance_standard_deviation=>1.9636275953788493, 
-				:distortion_standard_deviation=>9.070725194578191, 
-				:evasion_variance=>1.1947222222222225, 
-				:performance_variance=>3.8558333333333223, 
+				:evasion=>[3.9, 3.5, 5.7, 2.4, 2.5, 3.5],
+				:performance=>[87.4, 91.6, 86.5, 89.3, 91.8, 88.9],
+				:distortion=>[9.8, 29.7, 33.8, 14.2, 27.5, 31.5],
+				:evasion_average=>3.5833333333333335,
+				:performance_average=>89.25,
+				:distortion_average=>24.416666666666668,
+				:evasion_standard_deviation=>1.0930334954713063,
+				:performance_standard_deviation=>1.9636275953788493,
+				:distortion_standard_deviation=>9.070725194578191,
+				:evasion_variance=>1.1947222222222225,
+				:performance_variance=>3.8558333333333223,
 				:distortion_variance=>82.27805555555555}
 
 			expect(@rate.instance_variable_get(:@rate_hash)).to eq(expected_hash_result)
