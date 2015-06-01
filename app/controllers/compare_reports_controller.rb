7de4448aeb0383eb4334b8a1723de8c9fa0a249e
ThiagoBernardes <tmb0710@gmail.com>
2015-05-31 21:20:24 -0300

    Improving code quality, removing duplication in request_comparation
    Signed-off-by: PedroHenriqueSales <pedrosal3s@gmail.com>
    Signed-off-by: Thiago Bernardes <tmb0710@gmail.com>
    Signed-off-by: Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>

--- app/controllers/compare_reports_controller.rb
+++ app/controllers/compare_reports_controller.rb
@@ -8,11 +8,8 @@ class CompareReportsController < ApplicationController
 
 	def request_comparation
 
-		@first_report = ReportHelper.verify_params_of_test_type(params[:first_year],params[:grade],params[:first_state],params[:first_test_type],params[:first_public_type],params[:first_local])
-		@first_report.request_report
-
-		@second_report = ReportHelper.verify_params_of_test_type(params[:second_year],params[:grade],params[:second_state],params[:second_test_type],params[:second_public_type],params[:second_local])
-		@second_report.request_report
+		@first_report = create_report_to_compare(1)
+		@second_report = create_report_to_compare (2)
 
 		@comparation_hash = {:first_report => @first_report.report_result_hash,
 		 :second_report => @second_report.report_result_hash}
@@ -23,5 +20,25 @@ class CompareReportsController < ApplicationController
 
 	end
 
+
+
+	def create_report_to_compare(report_number)
+
+		if(report_number == 1)
+			@report = ReportHelper.verify_params_of_test_type(params[:first_year],params[:grade],params[:first_state],params[:first_test_type],params[:first_public_type],params[:first_local])
+		else
+      @report = ReportHelper.verify_params_of_test_type(params[:second_year],params[:grade],params[:second_state],params[:second_test_type],params[:second_public_type],params[:second_local])
+		end
+
+		@report.request_report
+
+		return @report
+
+	end
+
+
+
 end
+
+
 
