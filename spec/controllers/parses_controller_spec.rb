da15082d98ea9ddb768283900976ff82fb4e0962
João Vítor de Brito Fernandes <fernandesjvb@gmail.com>
2015-06-02 03:46:58 -0300

    Testing ideb results

--- spec/controllers/parses_controller_spec.rb
+++ spec/controllers/parses_controller_spec.rb
@@ -1,12 +1,12 @@
 require 'rspec'
 require 'rails_helper'
-
+# :nocov:
 describe ParsesController do
 
   before :each do
     @parses = Parse.all
   end
 
-
+# :nocov:
 
 end
--- spec/models/ideb_spec.rb
+++ spec/models/ideb_spec.rb
@@ -9,10 +9,23 @@ describe Ideb do
 
 describe "#initialize"
   it "take the params and set them to the object" do
-     expect(@ideb.instance_variable_get(:@year)).to eq(2011)
-     expect(@ideb.instance_variable_get(:@state_id)).to eq(1)
-     expect(@ideb.instance_variable_get(:@grade_id)).to eq(1)
+		expect(@ideb.instance_variable_get(:@year)).to eq(2011)
+		expect(@ideb.instance_variable_get(:@state_id)).to eq(1)
+		expect(@ideb.instance_variable_get(:@grade_id)).to eq(1)
+		expect(@ideb.instance_variable_get(:@ideb_result)).to eq([])
+		expect(@ideb.instance_variable_get(:@years)).to eq([])
+		expect(@ideb.instance_variable_get(:@grade_ids)).to eq([])
   end
 
+describe "#generate_hash_result"
+  it "should compute variance to given ideb" do
 
+  	expected_hash = {:status=>"available", :ideb=>[4.6], :ideb_average=>4.6, :ideb_standard_deviation=>0.0, 
+  		:ideb_variance=>0.0, :ideb_years=>["2011"], :ideb_grade_ids=>["1"]}
+
+  	@ideb.request_ideb_report
+  	expect(@ideb.instance_variable_get(:@ideb_hash)).to eq(expected_hash)
+  	
+  	#by testing this method, we make sure that the requested methods are also working
+  end
 end
