1d00320b967a6f04f2d3802b86885a11ddf76165
PedroSales <pedrosal3s@gmail.com>
2015-06-10 17:53:55 -0300

    Implementing model ranking with methods to request distortion, rates and ideb

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -1,38 +1,61 @@
 class Ranking
 
-	def initialize(year, grade, test_type, local, rate)
+	attr_accessor :ranking_result_hash
+
+	def initialize(year, grade)
 		@year = year
-		@grade = grade
-		@test_type = test_type
-		@local = local
-		@rate = rate
-		@lista = Array.new
+		@grade_id = Grade.grade_id_by_description(grade)
+		@test_type = "Total"
+		@local = "Total"
+		# @rate = rate
+
+		@listaDis = Array.new
+		@listaRat = Array.new
+		@listaIdeb = Array.new
 	end 
 
 	def insert_it
-
-		(states).each do |currentState|
-
-			currentReport = Report.new(@year, @grade, currentState, @test_type, @local)
-			currentReport.request_report
-
-			lista.push(currentReport)
+		if @grade_id.to_i <= 5
+			@current_type = "early"
+		else
+			@current_type = "final"
 		end
+		(State.all.pluck(:id).uniq.sort!).each do |currentState|
+			
+			currentDistortion = request_distortion(@year,currentState, @grade_id,@test_type,@local)
+			currentRates = request_rates(@year,currentState, @grade_id,@test_type,@local)
+			currentIdeb = request_ideb(@year,currentState, @current_type)
+	
+			@listaDis.push(currentDistortion)
+			@listaRat.push(currentRates)
+			@listaIdeb.push(currentIdeb)
+		end
+
+		@listaIdeb.sort_by!{|ideb| ideb.score}
+		@listaPeformance = @listaRat.sort_by!{|rates| rates.peformance}
+		@listaEvasion = @listaRat.sort_by!{|rates| rates.evasion}
+		@listaDistortion = @listaDis.sort_by!{|distortion| distortion.distortion}
+
+		@ranking_result_hash = {
+			:ideb_list => @listaIdeb,
+			:peformance_list => @listaPeformance,
+			:evasion_list => @listaEvasion,
+			:distortion_list => @listaDistortion
+		}
 	end
 
-	def sort_it
-
-		if @rate == "IDEB"
-			lista.sort_by {|report| report[:ideb]}
-		else 
-			if @rate = "Evasão"
-			lista.sort_by {|report| report[:evasion]}
-		else 
-			if @rate = "Rendimento"
-			lista.sort_by {|report| report[:performance]}
-		else 
-			if @rate = "Distorção de idade"
-			lista.sort_by {|report| report[:distortion]}
-		end
+	def request_distortion(year,state_id,grade_id,test_type,local)
+		Distortion.where(:year => year, :state_id => state_id, :local => local, :test_type => test_type,
+			:grade_id => grade_id).first
 	end
+
+	def request_rates(year,state_id,grade_id,test_type,local)
+		 Rate.where(:year => year, :state_id => state_id, :local => local, :test_type => test_type,
+		 :grade_id => grade_id).first
+	end
+
+	def request_ideb(year,state_id,type)
+		Ideb.where(:year => year, :state_id => state_id, :test_type => type).first
+	end
+
 end
