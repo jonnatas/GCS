a90ade96247516868298c7ce494e1acf76661542
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-06-20 11:22:11 -0300

    changing the view and adding subject to the model Signed-off-by: Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com> Signed-off-by: João Vítor de Brito Fernandes <fernandesjvb@gmail.com>

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,17 @@
+require 'open-uri'
+
 class Contact < MailForm::Base
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
   attribute :subject , :validade => false
+
+ def internet_connection?
+    begin
+      true if open("http://www.google.com/")
+    rescue
+      false
+    end
+ end
+
 end
