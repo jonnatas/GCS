49097ed0f40df0346819b5e3ed6ce9f49ec3887d
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2015-06-19 22:08:03 -0300

    Adding tests to contact controller Signed-off-by: Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com> Signed-off-by: João Vítor de Brito Fernandes <fernandesjvb@gmail.com>

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -23,13 +23,29 @@ Rails.application.configure do
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
+  config.action_mailer.raise_delivery_errors = false
+
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
+
+  ActionMailer::Base.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => "gmail.com",
+  :user_name => "contatoenturma@gmail.com",
+  :password => "contatoenturmaMDS",
+  :authentication => "plain",
+  :enable_starttls_auto => true
+
+}
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
