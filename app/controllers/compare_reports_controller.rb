232cd7d06fec47db8bf0b8968a2d13d5448a969b
ThiagoBernardes <tmb0710@gmail.com>
2015-06-25 17:59:30 -0300

    Decreasing duplication mess

--- app/controllers/compare_reports_controller.rb
+++ app/controllers/compare_reports_controller.rb
@@ -8,8 +8,8 @@ class CompareReportsController < ApplicationController
 
 	def request_comparation
 
-		@first_report = create_report_to_compare(1)
-		@second_report = create_report_to_compare (2)
+		@first_report = create_report_to_compare(params[:first_year],params[:first_state],params[:first_test_type],params[:first_public_type],params[:first_local])
+		@second_report = create_report_to_compare(params[:second_year],params[:second_state],params[:second_test_type],params[:second_public_type],params[:second_local])
 
 		@comparation_hash = {:first_report => @first_report.report_result_hash,
 		 :second_report => @second_report.report_result_hash}
@@ -22,14 +22,8 @@ class CompareReportsController < ApplicationController
 
 
 
-	def create_report_to_compare(report_number)
-
-		if(report_number == 1)
-			@report = ReportHelper.verify_params_of_test_type(params[:first_year],params[:grade],params[:first_state],params[:first_test_type],params[:first_public_type],params[:first_local])
-		else
-      @report = ReportHelper.verify_params_of_test_type(params[:second_year],params[:grade],params[:second_state],params[:second_test_type],params[:second_public_type],params[:second_local])
-		end
-
+	def create_report_to_compare(year, state, test_type, public_type, local)
+		@report = ReportHelper.verify_params_of_test_type(year,params[:grade],state,test_type,public_type,local)
 		@report.request_report
 
 		return @report
